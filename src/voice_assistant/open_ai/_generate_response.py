144ecfae5f101edeb242405699ef0e7a5eb3b679
chrisPickford1 <drchrispickford@gmail.com>
2023-05-20 07:33:49 +0100

    added functions

--- src/voice_assistant/open_ai/_generate_response.py
+++ src/voice_assistant/open_ai/_generate_response.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import openai
 
 from ._base import IGPTResponseGenerator
@@ -9,3 +10,19 @@ class GPTResponseGenerator(IGPTResponseGenerator):
 
     def submit(self, query: str) -> str:
         pass
+=======
+class GPTResponseGenerator(IGPTResponseGenerator):
+    def __init__(self):
+        pass
+
+    def submit(self, query: str) -> str:
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            propmpt=query,
+            max_tokens=4000,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+        return response["choices"][0]["text"]
+>>>>>>> Stashed changes
--- src/voice_assistant/text_to_speach/_transcriber.py
+++ src/voice_assistant/text_to_speach/_transcriber.py
@@ -6,7 +6,15 @@ from ._base import ITranscriber
 
 class AudioTranscriber(ITranscriber):
     def __init__(self):
+        self.engine = pyttsx3.init()
         self.recogniser = sr.Recognizer()
 
     def transcribe(self, file: str) -> str:
-        pass
+        with sr.AudioFile(file) as audio:
+            clip = self.recogniser.record(audio)
+
+        try:
+            return self.recogniser.recognize_google(clip)
+
+        except:
+            print("Could not recognise audio")
